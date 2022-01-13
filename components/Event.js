08c5fb716eeac2c9a0eadba923acc75828b7b24e
Alex <reactb0y1@gmail.com>
2022-01-13 16:48:57 +0200

    fix path, remove excess props, components

--- components/Event.js
+++ components/Event.js
@@ -1,28 +1,14 @@
 import Link from 'next/link';
-import Image from 'next/image';
-//to use Image with an external url, add some config on next.config.js
-//for more info, check out these docs https://nextjs.org/docs/basic-features/image-optimization
 
-export default function Event({ event, featuredMedia }) {
+export default function Event({event}) {
     return (
-        <div className="card mb-3" style={{ width: '18rem' }}>
-            <Link href={`/events/${event.slug}`}>
-                <a>
-                    <Image
-                        src={featuredMedia['media_details'].sizes.medium['source_url']}
-                        width={288}
-                        height={190}
-                        alt={featuredMedia['alt_text']}
-                        className="card-img-top"
-                    />
-                </a>
-            </Link>
+        <div className="card mb-3" style={{width: '18rem'}}>
             <div className="card-body">
-                <h5 className="card-title">{event.title.rendered}</h5>
-                <div
-                    className="card-text"
-                    dangerouslySetInnerHTML={{ __html: event.excerpt.rendered }}
-               />
+                <Link href={`/events/${event.slug}`}>
+                    <a>
+                        <h5 className="card-title">{event.title.rendered}</h5>
+                    </a>
+                </Link>
                 <p className="card-text">
                     <small className="text-muted">{event.acf.date}</small>
                 </p>
