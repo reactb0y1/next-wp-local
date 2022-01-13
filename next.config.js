6517f4643712fed7856c7200a1bfb6aa9ce97ce7
Alex <reactb0y1@gmail.com>
2022-01-13 16:22:42 +0200

    Paste code

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    //enter the domain or subdomain where you have WordPress installed
+    domains: ['my-wordpress.sportslink.info'],
+  },
 }
