c502fdb3103a08916b8d2008446e4d3654484681
Alex <reactb0y1@gmail.com>
2022-01-14 11:38:10 +0200

    fix path: /post -> /posts

--- components/Post.tsx
+++ components/Post.tsx
@@ -5,7 +5,6 @@ export default function Post({ post }) {
   return (
     <div className='card mb-3' style={{ maxWidth: '540px' }}>
       <div className='row g-0'>
-        <div className='col-md-4'></div>
         <div className='col-md-8'>
           <div className='card-body'>
             <Link href={`/posts/${post.slug}`}>
