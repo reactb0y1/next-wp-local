387f1ac2761ef1e3cb8dbcf56f37080ac277fac6
Alex <reactb0y1@gmail.com>
2022-01-20 17:59:30 +0200

    Icon

--- next.config.js
+++ next.config.js
@@ -4,4 +4,15 @@ module.exports = {
     //enter the domain or subdomain where you have WordPress installed
     domains: ['my-wordpress.sportslink.info'],
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack', 'file-loader'],
+    });
+
+    return config;
+  },
 };
---
+++ src/components/Icon/icon.component.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ReactComponent as FB } from '../../assets/images/fb.svg';
+import { ReactComponent as Twitter } from '../../assets/images/twitter.svg';
+import { ReactComponent as Instagram } from '../../assets/images/instagram.svg';
+
+type ComponentSvg = React.FC<React.SVGProps<SVGSVGElement>>;
+
+interface IProps extends React.SVGProps<SVGSVGElement> {
+  variant: IconVariant;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+type IconVariantComponents = {
+  [T in IconVariant]: ComponentSvg;
+};
+
+export type IconVariant = 'fb' | 'twitter' | 'instagram';
+
+const variantComponents: IconVariantComponents = {
+  fb: FB,
+  twitter: Twitter,
+  instagram: Instagram,
+};
+
+export const Icon = (props: IProps) => {
+  const Component = variantComponents[props.variant];
+  return Component ? <Component {...props} /> : null;
+};
