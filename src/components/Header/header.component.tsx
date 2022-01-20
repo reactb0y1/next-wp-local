9b3428c09498adf44ac52faa44b967a440f416eb
Alex <reactb0y1@gmail.com>
2022-01-20 16:31:32 +0200

    Header

--- src/components/Header/header.component.tsx
+++ src/components/Header/header.component.tsx
@@ -1,8 +1,60 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export const Header = () => {
-  return <StyledHeader className={'header'}>Header</StyledHeader>;
+  return (
+    <StyledHeader className={'header'}>
+      <div className='container'>
+        <div className='logo'>
+          <Link href={'/'}>
+            <a>
+              <Image src={'/logo.png'} width={243} height={111} />
+            </a>
+          </Link>
+        </div>
+
+        <div className='menu'>
+          <Link href={'/about'}>
+            <a>ABOUT US</a>
+          </Link>
+          <Link href={'/faq'}>
+            <a>FAQs</a>
+          </Link>
+          <Link href={'/support'}>
+            <a>SUPPORT</a>
+          </Link>
+          <Link href={'/contacts'}>
+            <a>CONTACTS</a>
+          </Link>
+        </div>
+      </div>
+    </StyledHeader>
+  );
 };
 
-const StyledHeader = styled.header``;
+const StyledHeader = styled.header`
+  height: 189px;
+
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .menu {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      gap: 88px;
+
+      a {
+        font-style: normal;
+        font-weight: bold;
+        font-size: 20px;
+        line-height: 135%;
+        color: #515bdf;
+      }
+    }
+  }
+`;
