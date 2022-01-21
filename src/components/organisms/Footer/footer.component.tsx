ade88cf9e77a764ff8df353de07ca5dc02ac887f
Alex <reactb0y1@gmail.com>
2022-01-21 17:19:39 +0200

    Footer: base styling

--- src/components/organisms/Footer/footer.component.tsx
+++ src/components/organisms/Footer/footer.component.tsx
@@ -19,12 +19,14 @@ export const Footer = () => {
             <Link href={'#'}>
               <a>Support</a>
             </Link>
-            <Link href={'#'}>
-              <a>Support</a>
-            </Link>
-            <Link href={'#'}>
-              <a>Support</a>
-            </Link>
+            <div className='sub-menu'>
+              <Link href={'#'}>
+                <a>Terms of Use</a>
+              </Link>
+              <Link href={'#'}>
+                <a>Privacy Policy</a>
+              </Link>
+            </div>
           </li>
           <li>
             <Link href={'#'}>
@@ -35,10 +37,11 @@ export const Footer = () => {
             <Link href={'#'}>
               <a>Contacts</a>
             </Link>
-
-            <Link href={'#'}>
-              <a>info@givvy.com</a>
-            </Link>
+            <div className='sub-menu'>
+              <Link href={'#'}>
+                <a>info@givvy.com</a>
+              </Link>
+            </div>
           </li>
         </ul>
         <div className='subscribe'>Subscribe</div>
@@ -50,24 +53,56 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  border-top: 1px solid #515bdf;
+  border-top: 1px solid var(--c-primary);
   padding: 67px 0 47px;
 
   .container {
     display: grid;
-    grid-template-columns: 1fr repeat(4, minmax(0, 1fr));
+    grid-template-columns: 300px repeat(4, minmax(0, 1fr));
     grid-template-rows: repeat(3, auto);
-    gap: 20px;
+    row-gap: 20px;
+    column-gap: 100px;
     width: 1290px;
     margin: 0 auto;
   }
 
+  .menu li > a,
+  .download-app .title,
+  .social .title {
+    font-style: normal;
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 32px;
+    color: var(--c-primary);
+  }
+
   .download-app {
     grid-area: span 2;
+
+    .title {
+      margin-bottom: 64px;
+    }
   }
 
   .menu {
+    display: grid;
     grid-area: auto / span 4;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    row-gap: 20px;
+    column-gap: 100px;
+
+    li > a {
+      margin-bottom: 30px;
+    }
+
+    .sub-menu {
+      font-weight: normal;
+      font-size: 20px;
+      line-height: 200%;
+      color: var(--c-primary);
+      display: flex;
+      flex-direction: column;
+    }
   }
 
   .subscribe {
@@ -76,9 +111,16 @@ const StyledFooter = styled.footer`
 
   .social {
     grid-area: 2 / -2 / -2 / -1;
+
+    .title {
+      margin-bottom: 38px;
+    }
   }
 
   .copyright {
     grid-area: -2 / 1 / -1 / 2;
+    font-size: 12px;
+    line-height: 146.4%;
+    color: var(--c-primary);
   }
 `;
