e0aece3af57c2ace0f1d5f4c4d03229a61b501d6
Alex <reactb0y1@gmail.com>
2022-01-25 15:32:57 +0200

    Integrate: AboutUs, list

--- src/components/organisms/AboutUs/about-us.component.tsx
+++ src/components/organisms/AboutUs/about-us.component.tsx
@@ -11,103 +11,14 @@ export const AboutUs = ({ about_us }) => {
       </div>
 
       <ul className='list'>
-        <li className='item'>
-          <div className='img-wrap'>
-            <Image
-              src={'/about-us-1.jpg'}
-              layout={'fill'}
-              objectFit={'contain'}
-              className={'image'}
-            />
-          </div>
-          <div className='text'>
-            <p>
-              Givvy was designed from a desire to have a better relationship and closer connection
-              to the people we care about in a world where we don’t see each other as often as we’d
-              like anymore. givvy’s not about the gift, givvy’s about the experience of giving the
-              gift; something real in a digital world and the way it makes people feel important and
-              cared about. We believe the best gift experiences involve the surprise of opening
-              something special, not a gift card or just checking a box on a registry. We believe
-              giving a great gift is when the people you care about know you care enough to give
-              them something thoughtful they’ll cherish.
-            </p>
-            <p className={'active'}>
-              Everybody has that one friend who is amazing at gifting. The friend who never forgets
-              an occasion and sends you a Valentine’s Day card even though you’re single. Even
-              something as simple as a card can make a person smile and feel loved and remembered!
-            </p>
-          </div>
-        </li>
-        <li className='item reverse'>
-          <div className='img-wrap'>
-            <Image
-              src={'/about-us-2.jpg'}
-              layout={'fill'}
-              objectFit={'contain'}
-              className={'image'}
-            />
-          </div>
-          <div className='text'>
-            <p>
-              Unfortunately, we have busy lives at home and careers and like so many people, finding
-              the time for shopping can be daunting. Then there’s the issue of finding a good gift
-              because that in and of itself can feel impossible for many people. When we started
-              givvy, we heard from many different people from all kinds of backgrounds who dreaded
-              the holiday season just because of the shopping.
-            </p>
-            <p className={'active'}>
-              If only we all had that professional personal assistant working for an executive we
-              see on TV. A personal assistant who keeps track of all the occasions, somehow knows
-              what everybody likes. We wanted a personal assistant who makes sure the people you
-              care about know you think about them all the time.
-            </p>
-          </div>
-        </li>
-        <li className='item'>
-          <div className='img-wrap'>
-            <Image
-              src={'/about-us-3.jpg'}
-              layout={'fill'}
-              objectFit={'contain'}
-              className={'image'}
-            />
-          </div>
-          <div className='text'>
-            <p>
-              So we designed and built givvy to be the personal assistant you needed. We know how
-              busy life can get so we built a slick interface and notification system so you never
-              forget. We know how overwhelming it was on other apps to manually build contact
-              profiles so we made it fast and easy for your contacts to easily complete their own
-              private profiles. We know many people don’t feel confident in finding great gifts so
-              we hired professional shoppers to find those gifts for you.
-            </p>
-            <p>
-              We know all the best gifts wouldn’t do our users any good if we couldn’t match them up
-              so we made an awesome algorithm. We also know seeing an endless list of gifts is no
-              different from shopping so we narrow it down and show you only a handful of the best
-              of the best for your price range and contact. We built everything from the start to
-              finish wanting to strengthen your relationships, build memorable experiences, and to
-              send the message you care.
-            </p>
-          </div>
-        </li>
-        <li className='item full'>
-          <div className='img-wrap'>
-            <Image
-              src={'/about-us-4.jpg'}
-              layout={'fill'}
-              objectFit={'contain'}
-              className={'image'}
-            />
-          </div>
-          <div className='text'>
-            <p>
-              That’s what givvy is about. It’s about making the people we care about feel special
-              and loved. It’s about all of us having the opportunity to be great at gifting in just
-              2 minutes.
-            </p>
-          </div>
-        </li>
+        {about_us.list.map(({ image, text }, index) => (
+          <li className='item' key={index}>
+            <div className='img-wrap'>
+              <Image src={image} layout={'fill'} objectFit={'contain'} className={'image'} />
+            </div>
+            <div className='text' dangerouslySetInnerHTML={{ __html: text }} />
+          </li>
+        ))}
       </ul>
     </StyledAboutUs>
   );
@@ -163,7 +74,7 @@ const StyledAboutUs = styled.section`
   }
 
   .item {
-    &.reverse {
+    &:nth-child(2n) {
       & > *:first-child {
         order: 1;
 
@@ -176,7 +87,7 @@ const StyledAboutUs = styled.section`
       }
     }
 
-    &.full {
+    &:last-child {
       grid-template-columns: 1fr;
       grid-template-rows: auto auto;
       gap: 117px;
@@ -235,7 +146,8 @@ const StyledAboutUs = styled.section`
       font-size: 24px;
       line-height: 156.5%;
 
-      &.active {
+      strong {
+        font-weight: 500;
         color: var(--c-primary);
       }
 
