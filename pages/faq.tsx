2efa62b9eedb957565f21732400d03f891009a14
Alex <reactb0y1@gmail.com>
2022-01-25 13:19:40 +0200

    Integrate: FAQ page, faq

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { MainFAQ } from '../src/components/templates/MainFAQ';
 import { Page } from '../src/components/templates/Page';
 
-export default function FAQ() {
+export default function FAQ({ faq }) {
   return (
     <Page title={'FAQ'}>
-      <MainFAQ />
+      <MainFAQ faq={faq} />
     </Page>
   );
+}
+
+export async function getStaticProps() {
+  const postsRes = await fetch('http://next-wp-local/wp-json/acf/v3/pages/23');
+  const posts = await postsRes.json();
+  const faq = posts.acf.faq;
+
+  return {
+    props: {
+      faq,
+    },
+  };
 }
--- src/components/templates/MainFAQ/main-faq.component.tsx
+++ src/components/templates/MainFAQ/main-faq.component.tsx
@@ -3,45 +3,7 @@ import styled from 'styled-components';
 import { Icon } from '../../atoms/Icon';
 import Image from 'next/image';
 
-const questions_mock = [
-  {
-    id: 0,
-    title: 'The distinction',
-    question: 'What is Givvy?',
-    answer:
-      'Givvy isn’t a shopping app, a gift registry or a generic gift recommendation app. Givvy uses professional shoppers dedicated to finding unique and meaningful gifts which are perfect for your budget, the specific occasion, and person you’re gifting. Best of all, we only show the very best 8 options so you never run into a stressful shopping experience.\n',
-  },
-  {
-    id: 1,
-    title: 'Availability',
-    question: 'Is givvy affordable?',
-    answer:
-      'Absolutely! Givvy may use professional shoppers to populate our system, but an advanced algorithm chooses the right gifts for your occasion, and that means you get the absolute best of both worlds. Great gifts chosen by professional shoppers without having to spend the money to hire a professional shopper yourself.',
-  },
-  {
-    id: 2,
-    title: 'Professional Approach',
-    question: 'Does givvy have a good gift for every occasion? ',
-    answer:
-      'Yes! If our algorithm can’t find a great match for your occasion, we automatically assign a real professional shopper from our team to work on your occasion personally at no charge. We’ll get your personal gift recommendations loaded up within about 24 hours. We’re committed to ensuring you always have a great experience!',
-  },
-  {
-    id: 3,
-    title: 'Safety',
-    question: 'Is my data safe?',
-    answer:
-      'Definitely. We’ve always been strongly committed to data security and we’ve hired independent system security professionals to test our systems starting months before we even began beta testing. After all, the founders of Givvy use the app, too!',
-  },
-  {
-    id: 4,
-    title: 'Privacy',
-    question: 'Does givvy share or sell my information?',
-    answer:
-      'Givvy never sells any private profile information and we share information only as needed to operate our service. We take privacy very seriously. Check out our privacy policy here!',
-  },
-];
-
-export const MainFAQ = () => {
+export const MainFAQ = ({ faq }) => {
   return (
     <StyledMainFAQ className={'main'}>
       <section className={'banner'}>
@@ -53,8 +15,8 @@ export const MainFAQ = () => {
       </section>
       <section className='list-section'>
         <ul className='list'>
-          {questions_mock.map(({ id, title, question, answer }) => (
-            <li className={'item'} key={id}>
+          {faq.map(({ title, question, answer }, index) => (
+            <li className={'item'} key={index}>
               <p className='title'>{title}</p>
               <div className='content'>
                 <h2 className='question'>{question}</h2>
