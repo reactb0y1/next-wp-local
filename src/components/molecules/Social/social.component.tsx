c12176b179a4a83b98fda4c9d7d4d027b8997956
Alex <reactb0y1@gmail.com>
2022-01-28 10:57:27 +0200

    Integrate: Social

--- src/components/molecules/Social/social.component.tsx
+++ src/components/molecules/Social/social.component.tsx
@@ -1,39 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Icon } from '../../atoms/Icon';
 import Link from 'next/link';
+import { useStore } from 'effector-react';
+import { $store } from '../../../stores/global/global.store';
+import Image from 'next/image';
 
-interface ISocial {
-  title?: string;
-}
+export const Social = () => {
+  const store = useStore($store);
 
-export const Social = ({ title }: ISocial) => {
   return (
     <StyledSocial className={'social'}>
-      {title && <p className={'title'}>{title}</p>}
+      <p className={'title'}>{store?.components?.social?.title}</p>
 
       <div className='links'>
-        <Link href={'#'}>
-          <a>
-            <Icon variant={'fb'} width={16} height={29} />
-          </a>
-        </Link>
-        <Link href={'#'}>
-          <a>
-            <Icon variant={'twitter'} width={34} height={28} />
-          </a>
-        </Link>
-        <Link href={'#'}>
-          <a>
-            <Icon variant={'instagram'} width={28} height={28} />
-          </a>
-        </Link>
+        {store?.components?.social?.list?.map(({ name, link, ico }, index) => {
+          return (
+            <Link href={link} key={index}>
+              <a>
+                <Image src={ico} width={34} height={28} alt={`Logo`} />
+              </a>
+            </Link>
+          );
+        })}
       </div>
     </StyledSocial>
   );
 };
 
 const StyledSocial = styled.div`
+  .title {
+    .header & {
+      display: none;
+    }
+  }
+
   .links {
     display: flex;
     align-items: center;
