99f364baff3e51c8d49b129dd5c613b7ffde0b93
Alexandr <reactb0y1@gmail.com>
2022-05-07 22:29:32 +0300

    Counter: useSelector(), useDispatch()

--- src/components/molecules/Counter/counter.component.tsx
+++ src/components/molecules/Counter/counter.component.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
+import {useDispatch, useSelector} from "react-redux";
 
 export const Counter = () => {
+    // @ts-ignore
+    const count = useSelector(state => state.testData);
+    const dispatch = useDispatch();
+
     return (
         <StyledCounter>
-            <button>+</button>
-            <p>0</p>
-            <button>-</button>
+            <button
+                onClick={() => {
+                    dispatch({type: "ADD"})
+                }}
+            >
+                +
+            </button>
+            <p>{count}</p>
+            <button
+                onClick={() => {
+                    dispatch({type: "REMOVE"})
+                }}
+            >
+                -
+            </button>
         </StyledCounter>
     );
 };
@@ -14,7 +31,7 @@ export const Counter = () => {
 const StyledCounter = styled.div`
   display: flex;
   gap: 10px;
-  
+
   button {
     padding: 5px;
   }
