baf0e9428abea890d78366f5696ac4828bee557b
Alexandr <reactb0y1@gmail.com>
2022-05-07 21:02:13 +0300

    _app.tsx: add store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,26 @@
 import '../src/styles/globals.scss';
 import Head from 'next/head';
+import {store, wrapper} from '../src/store/store';
+import {Provider} from 'react-redux';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-    beta3/dist/css/bootstrap.min.css'
-          rel='stylesheet'
-          integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6'
-          crossOrigin='anonymous'
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+
+function MyApp({Component, pageProps}) {
+    return (
+        <>
+            <Provider store={store}>
+
+                <Head>
+                    <link
+                        href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-    beta3/dist/css/bootstrap.min.css'
+                        rel='stylesheet'
+                        integrity='sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6'
+                        crossOrigin='anonymous'
+                    />
+                </Head>
+                <Component {...pageProps} />
+            </Provider>
+        </>
+    );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
